830fa96204baf4444a2f52d5cedb76f61873f116
Sinerjit <sarp.cubukcuoglu@gmail.com>
2024-01-19 19:11:05 -0800

    coronavirus data analysis

--- data_add.py
+++ data_add.py
@@ -2,32 +2,48 @@ import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
 
-# CSV dosyasını oku ve hatalı satırları atla
-df = pd.read_csv('C:\\Users\\sarp_\\Desktop\\X\\Projects\\Data Analyst Portfolio Project 1\\CovidDeaths.csv', delimiter=';', on_bad_lines='skip')
+# Read the CSV file and skip faulty lines
+df = pd.read_csv('__csv_file_path__', delimiter=';', on_bad_lines='skip')
 
-# Veriyi incele
+# Explore the data
 # pd.set_option('display.max_columns', None)
 # pd.set_option('display.expand_frame_repr', False)
 # print(df)
+
 try:
+    # Establish a connection to the PostgreSQL database
     db_connection = psycopg2.connect(
         host="localhost",
-        database="CoronaVirus",
-        user="postgres",
-        password="1S2a3r4p",
-        port="5433"  # PostgreSQL sunucunuzun kullandığı port numarası
+        database="__database_name__",
+        user="__username__",
+        password="__password__",
+        port="__port_number__"
     )
 
- # Burada bağlantı üzerinde çalışmalar yapabilirsiniz.
-except Exception as e:
-    print(f"Error: {e}")
-finally:
+
+    # Check if the database connection is established
     if db_connection:
+        # If connected, print a message indicating a successful connection
         print("Connected")
+
+        # Create a SQLAlchemy engine for PostgreSQL
+        engine = create_engine('postgresql://postgres:password@localhost:port_number/CoronaVirus')
+
+        # Write the DataFrame to the PostgreSQL table 'covid_vaccinations'
+        df.to_sql('covid_vaccinations', con=engine, if_exists='append', index=False)
+
+        # Close the database connection
         db_connection.close()
 
+except Exception as e:
+    # If there is an exception during the database connection attempt, print a more specific error message
+    print(f"Error during database connection: {e}")
+
+# Create a SQLAlchemy engine for PostgreSQL
 engine = create_engine('postgresql://postgres:1S2a3r4p@localhost:5433/CoronaVirus')
 
-df.to_sql('covid_deaths', con=engine, if_exists='append', index=False)  # 'replace' yerine 'append' de kullanabilirsiniz
+# Write the DataFrame to the PostgreSQL table 'covid_vaccinations'
+df.to_sql('covid_vaccinations', con=engine, if_exists='append', index=False)  # 'replace' yerine 'append' de kullanabilirsiniz
 
+# Close the database connection
 db_connection.close()
